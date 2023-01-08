c8495fcc7cbbd4e5234adb4c7fd05657996a14fe
Max Deutsch <max.deutsch@postoe.at>
2023-01-08 17:25:44 +0100

    feat: reset camera. refactor input with input map

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -8,19 +8,19 @@ var dragging = false
 
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == 4:
-			zoom -= Vector2(1, 1)
-		elif event.button_index == 5:
-			zoom += Vector2(1, 1)
-
+	if event.is_action("reset_view"):
+		zoom = Vector2(1,1)
+		position = Vector2(0,0)
+	elif event.is_action("zoom_in"):
+		zoom -= Vector2(1, 1)
+	elif event.is_action("zoom_out"):
+		zoom += Vector2(1, 1)
+	elif event.is_action("drag"):
 		if event.is_pressed():
 			mouse_start_pos = event.position
 			screen_start_position = position
 			dragging = true
 		else:
-			dragging = false
-		
-		
+			dragging = false		
 	elif event is InputEventMouseMotion and dragging:
 		position = zoom * (mouse_start_pos - event.position) + screen_start_position
