643c67dd01c4763fdef4529720671c20304dd359
Max Deutsch <max.deutsch@postoe.at>
2023-01-08 13:04:33 +0100

    feat: Read solar system data

--- scripts/SolarSystem.gd
+++ scripts/SolarSystem.gd
@@ -1,13 +1,28 @@
 extends Node2D
 
-var celestialBodyScene = preload("res://scenes/CelestialBody.tscn");
+const celestialBodyScene = preload("res://scenes/CelestialBody.tscn");
+const dataPath = "res://data/solar-system.csv"
 
 var celestialBodies = {}
 
-func _ready():	
+func _ready():
+	loadData()
+	print(celestialBodies)
 	addSun()
 	addPlanet('earth')
 
+func loadData():
+	var file = File.new()
+	file.open(dataPath, file.READ)
+	var header = file.get_csv_line(';')
+	while !file.eof_reached():
+		var row = file.get_csv_line(';')
+		if(row.size() > 1):
+			var name = row[0]
+			celestialBodies[name] = {}
+			for i in range(1, header.size()):
+				celestialBodies[name][header[i]] = float(row[i])
+		
 func addSun():
 	var sun = celestialBodyScene.instance()
 	sun.set_mode(1)
