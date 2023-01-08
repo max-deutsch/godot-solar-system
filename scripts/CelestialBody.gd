afb875d64b8f707f3c2a77488129c78415efb2ec
Max Deutsch <max.deutsch@postoe.at>
2023-01-08 17:06:06 +0100

    feat: Add data types to csv. Add color instead of sprite for the bodies.

---
+++ scripts/CelestialBody.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+var radius = 10
+var color = Color.deeppink
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _draw():
+	draw_circle(Vector2(0, 0), radius, color)
+	
--- scripts/SolarSystem.gd
+++ scripts/SolarSystem.gd
@@ -14,13 +14,23 @@ func loadData():
 	file.open(dataPath, file.READ)
 	var data = {}
 	var header = file.get_csv_line(';')
+	var types = file.get_csv_line(';')
 	while !file.eof_reached():
 		var row = file.get_csv_line(';')
 		if(row.size() > 1):
 			var name = row[0]
 			data[name] = {}
-			for i in range(1, header.size()):
-				data[name][header[i]] = float(row[i])
+			for i in range(1, header.size()):				
+				var value
+				match types[i]:
+					'string':
+						value = row[i]
+					'float':
+						value = float(row[i])
+					'int':
+						value = int(row[i])
+				data[name][header[i]] = value
+
 	return data
 
 func initBodies(data):
@@ -30,7 +40,9 @@ func initBodies(data):
 		add_child(body)
 
 func createBody(data):
-	var body: RigidBody2D = celestialBodyScene.instance()		
-	body.set_mode(int(data['static']))
+	var body = celestialBodyScene.instance()		
+	body.set_mode(data['mode'])
+	body.color = Color(data['color'])
 	body.set_position(Vector2(data['DistanceToSun[Mkm]'], 0))
+	body.get_node('Sprite')
 	return body
